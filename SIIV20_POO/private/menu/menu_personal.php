7bb1e706e488850aca0e349c21ff314d7be90a09
TheRussian7u7 <urielmedina7u7@outlook.com>
2025-04-27 03:35:36 -0600

    vercion 10.2

--- SIIV20_POO/private/menu/menu_personal.php
+++ SIIV20_POO/private/menu/menu_personal.php
@@ -132,8 +132,8 @@ class NavigationMenu
         echo "<nav class='navbar text-white shadow-sm' style='background-color: #1B396A;'>
                 <div class='container-fluid px-md-4'>
                     <div class='d-flex align-items-center text-white'>
-                        <span class='menu-personal'>{$this->SII}</span>
-                        <span class='menu-personal d-md-none'>{$this->SII_short}</span>
+                        <span class='SII-MENU-PERSONAL d-none d-md-inline'>{$this->SII}</span>
+                        <span class='SII-MENU-PERSONAL d-md-none'>{$this->SII_short}</span>
                     </div>
                     <button class='navbar-toggler bg-light ms-auto' type='button' data-bs-toggle='offcanvas' 
                             data-bs-target='#offcanvasNavbar' aria-controls='offcanvasNavbar' 
